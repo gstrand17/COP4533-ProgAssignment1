4a9387b864955ae5892408e77ec6f44fd0945825
gstrand17 <gstrand@ufl.edu>
2026-01-23 15:15:04 -0500

    matcher vers 1

--- matcher.cpp
+++ matcher.cpp
@@ -1,3 +1,36 @@
-//
-// Created by graci on 1/20/2026.
-//
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+#include <set>
+using namespace std;
+
+void matcher(map<int, vector<int>> hospitals, map<int, vector<int>> students, int n){
+    vector<pair<int, int>> matching;
+    set<int> available;
+    queue<int> pending;
+    // create a hospitals pending queue [1,2,3]
+    for (int i=1; i<=n; i++){
+        pending.push(i);
+        available.emplace(i);
+    }
+
+    while (!pending.empty()){
+        int num = pending.front();
+        for (int i = 0; i<n; i++){
+            if (available.count(hospitals[num][i])==0){
+                // student is free
+                matching.push_back({num, i}); // match made
+                pending.pop(); // hospital doesn't need a student any longer
+                available.erase(i); // student is no longer available
+            }
+            else if(){
+
+            }
+        }
+
+
+
+    }
+}
+

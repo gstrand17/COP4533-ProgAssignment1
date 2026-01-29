33fc48dff22d81f20f80ac8bab20ed268800de68
gstrand17 <gstrand@ufl.edu>
2026-01-29 12:55:44 -0500

    generator

--- main.cpp
+++ main.cpp
@@ -6,66 +6,71 @@
 #include <sstream>
 #include "matcher.cpp"
 #include "checker.cpp"
+#include "generator.cpp"
 using namespace std;
 
 
 int main() {
-    ifstream inFile ("../example.txt");
-    if (!inFile.is_open()){
-        cout << "Error opening file" << endl;
-        return -1;
-    }
-    string str_n; // parse n
-    getline(inFile, str_n);
-    cout << "string n is " << str_n << endl;
-    int n = stoi(str_n); //convert to int
-    cout << "int n = " << n << endl;
-    map<int, vector<int>> hospitals;
-    map<int, vector<int>> students;
+    vector<int> nArray{2, 256};
+    vector<string> fileList = generator(nArray);
+    for (string file : fileList){
+        ifstream inFile (file);
+        if (!inFile.is_open()){
+            cout << "Error opening file" << endl;
+            return -1;
+        }
+        string str_n; // parse n
+        getline(inFile, str_n);
+        cout << "string n is " << str_n << endl;
+        int n = stoi(str_n); //convert to int
+        cout << "int n = " << n << endl;
+        map<int, vector<int>> hospitals;
+        map<int, vector<int>> students;
 
-    string str;
-    for (int i = 1; i <= n; i++) {
-        vector<int> temp;
-        getline(inFile, str);
-        stringstream ss(str);
-        for (int j = 0; j < n; j++) {
-            getline(ss,str, ' ');
-            temp.push_back(stoi(str));
+        string str;
+        for (int i = 1; i <= n; i++) {
+            vector<int> temp;
+            getline(inFile, str);
+            stringstream ss(str);
+            for (int j = 0; j < n; j++) {
+                getline(ss,str, ' ');
+                temp.push_back(stoi(str));
+            }
+            hospitals[i] = temp;
         }
-        hospitals[i] = temp;
-    }
-    for (int i = 1; i <= n; i++) {
-        vector<int> temp;
-        getline(inFile, str);
-        stringstream ss(str);
-        for (int j = 0; j < n; j++) {
-            getline(ss,str, ' ');
-            temp.push_back(stoi(str));
+        for (int i = 1; i <= n; i++) {
+            vector<int> temp;
+            getline(inFile, str);
+            stringstream ss(str);
+            for (int j = 0; j < n; j++) {
+                getline(ss,str, ' ');
+                temp.push_back(stoi(str));
+            }
+            students[i] = temp;
         }
-        students[i] = temp;
-    }
-    for (auto n: hospitals) {
-        cout << "Hosptial: "<< n.first << " list: "; ;
-        for (int i = 0; i < n.second.size(); i++) {
-            cout << n.second[i] << " ";
+        for (auto n: hospitals) {
+            cout << "Hosptial: "<< n.first << " list: "; ;
+            for (int i = 0; i < n.second.size(); i++) {
+                cout << n.second[i] << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
-    }
-    for (auto n: students) {
-        cout << "Student: "<< n.first << " list: "; ;
-        for (int i = 0; i < n.second.size(); i++) {
-            cout << n.second[i] << " ";
+        for (auto n: students) {
+            cout << "Student: "<< n.first << " list: "; ;
+            for (int i = 0; i < n.second.size(); i++) {
+                cout << n.second[i] << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
-    }
-    vector<pair<int,int>> matched;
-    matched = matcher(hospitals, students, n);
-    bool stable = verify(matched, hospitals, students, n);
-    if (stable == 1) { // bool 1 = true
-        cout << "true & stable"<< endl;
-    }
-    else { //bool 0 = false
-        cout << "false & unstable" << endl;
+        vector<pair<int,int>> matched;
+        matched = matcher(hospitals, students, n);
+        bool stable = verify(matched, hospitals, students, n);
+        if (stable == 1) { // bool 1 = true
+            cout << "true & stable"<< endl;
+        }
+        else { //bool 0 = false
+            cout << "false & unstable" << endl;
+        }
     }
     return 0;
 }
